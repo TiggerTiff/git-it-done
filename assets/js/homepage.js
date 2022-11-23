34c14223a1f8fba32a7c680118fd491e2857596e
TiggerTiff <tiffanybell031@gmail.com>
2022-11-24 06:25:33 +1100

    redirect issue search to index.html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -24,7 +24,7 @@ var getUserRepos = function(user) {
 
 };
   
-
+// displayRepos
 var displayRepos = function(repos, searchTerm) {
    // check if api returned any repos
 if (repos.length === 0) {
@@ -41,10 +41,10 @@ for (var i = 0; i < repos.length; i++) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
   
-    // create a container for each repo
-    var repoEl = document.createElement("div");
+    // create a link for each repo
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-  
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
